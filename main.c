8a53954ea047772d9275206b96e475ed5b082145
rena0204 <rena0204@sookmyung.ac.kr>
2025-09-18 16:38:58 +0900

    03 (3)

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 
 int main(void) 
 {
-	char c;
-	int i;
+	char c, d;
 	
-	printf("input a number:");
+	printf("enter a character:");
 	scanf("%c",&c);
 	
-	i=c-'0';
-	printf("The input number is %i\n",i);
+	d=c+1;
+	printf("The next character of %c (%i) is %c (%i).\n",c,c,d,d);
 			
 	return 0;
 }
